d8ffce80fda3d18b7f7525ea06788fdb89e6bf0c
tvinnie <tvinnie77@gmail.com>
2024-06-05 07:49:31 +0300

    Update Fibonacci Numbers

--- Fibonacci Numbers/solution.py
+++ Fibonacci Numbers/solution.py
@@ -1,4 +1,17 @@
 # function to print fibonacci numbers 
+
+
+""" 
+They are a sequence of integer numbers built using a very simple rule:
+    the first element of the sequence is equal to one (Fib1 = 1)
+    the second is also equal to one (Fib2 = 1)
+    every subsequent number is the the_sum of the two preceding numbers:(Fibi = Fibi-1 + Fibi-2)
+    
+Here are some of the first Fibonacci numbers:
+    fib_1 = 1 fib_2 = 1 fib_3 = 1 + 1 = 2 fib_4 = 1 + 2 = 3 fib_5 = 2 + 3 = 5 fib_6 = 3 + 5 = 8 fib_7 = 5 + 8 = 13 
+"""
+
+
 def fib(n):
     if n < 1:
         return None
