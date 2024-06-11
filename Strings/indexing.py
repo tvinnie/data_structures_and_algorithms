00c2d30e1d17d3f831747163284dbc2492978b0b
tvinnie <tvinnie77@gmail.com>
2024-06-11 09:17:55 +0300

    Strings Manipulation

---
+++ Strings/indexing.py
@@ -0,0 +1,12 @@
+# Indexing strings.
+
+the_string = 'silly walks'
+
+for ix in range(len(the_string)):
+    print(the_string[ix], end=' ')
+
+print()
+
+
+# Output 
+# s i l l y   w a l k s
---
+++ Strings/iterating.py
@@ -0,0 +1,12 @@
+# Iterating through a string.
+
+the_string = 'silly walks'
+
+for character in the_string:
+    print(character, end=' ')
+
+print()
+
+
+# Output
+# s i l l y   w a l k s 
