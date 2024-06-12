9466f31e591421aa15ce9e670e3610d49aeb76b8
tvinnie <tvinnie77@gmail.com>
2024-06-12 08:03:11 +0300

    is alpha method

---
+++ Strings/is-alpha-method.py
@@ -0,0 +1,10 @@
+# NOTE: isalpha() method
+
+# The isalpha() method is more specialized – it's interested in LETTERS ONLY
+
+# Example 1: Demonstrating the isapha() method:
+print("Moooo".isalpha())
+print('Mu40'.isalpha())
+    
+# Output
+#  True False
