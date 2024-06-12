821cecf1334853b27932892e2e99d7f7381ff588
tvinnie <tvinnie77@gmail.com>
2024-06-12 08:43:16 +0300

    space and upper method

---
+++ Strings/is-space-method.py
@@ -0,0 +1,16 @@
+# NOTE: isspace() method
+
+# The isspace() method identifies whitespaces only – it disregards any other character (the result is False then).
+
+# Example: Demonstrating the isspace() method:
+print(' \n '.isspace())
+print(" ".isspace())
+print("mooo mooo mooo".isspace())
+
+# OUTPUT
+"""
+True
+True
+False
+
+"""
---
+++ Strings/is-upper-method.py
@@ -0,0 +1,11 @@
+# NOTE: isupper() method
+
+# The isupper() method is the upper-case version of islower() – it concentrates on upper-case letters only.
+
+# Example: Demonstrating the isupper() method:
+print("Moooo".isupper())
+print('moooo'.isupper())
+print('MOOOO'.isupper())
+
+# OUTPUT
+#  False False True
