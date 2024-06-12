6d8f587ac47778de43dae36e16ed7dc6c43eef10
tvinnie <tvinnie77@gmail.com>
2024-06-12 08:14:33 +0300

    is lower method

---
+++ Strings/is-lower-method.py
@@ -0,0 +1,10 @@
+# NOTE: islower() method
+
+# The islower() method is a fussy variant of isalpha() – it accepts lower-case letters only.
+
+# Example: Demonstrating the islower() method:
+print("Moooo".islower())
+print('moooo'.islower())
+
+# OUTPUT 
+# False True
