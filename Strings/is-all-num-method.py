b6bf3a887959d2c51e3862cabbee78e407da8319
tvinnie <tvinnie77@gmail.com>
2024-06-12 08:00:55 +0300

    is all num method

---
+++ Strings/is-all-num-method.py
@@ -0,0 +1,22 @@
+# NOTE: isalnum() method
+
+# The parameterless method named isalnum() checks if the string contains only digits or alphabetical characters (letters), and returns True or False according to the result.
+
+# Demonstrating the isalnum() method:
+print('lambda30'.isalnum())
+print('lambda'.isalnum())
+print('30'.isalnum())
+print('@'.isalnum())
+print('lambda_30'.isalnum())
+print(''.isalnum())
+
+# Output
+"""
+True
+True
+True
+False
+False
+False
+
+"""
