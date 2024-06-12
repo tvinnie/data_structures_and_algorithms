205028e85a2ebca71a5a1c9ced40eb2be7136aef
tvinnie <tvinnie77@gmail.com>
2024-06-12 08:11:55 +0300

    is digit method

---
+++ Strings/is-digit-method.py
@@ -0,0 +1,10 @@
+#  NOTE: isdigit() method
+
+# The isdigit() method looks at digits only – anything else produces False as the result.
+
+# Example 2: Demonstrating the isdigit() method:
+print('2018'.isdigit())
+print("Year2019".isdigit())
+
+# OUTPUT
+# True False
