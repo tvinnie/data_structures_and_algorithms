9afddfadc86a4523eece09d486cc534c94965be9
tvinnie <tvinnie77@gmail.com>
2024-06-12 08:48:43 +0300

    lower method

---
+++ Strings/lower-method.py
@@ -0,0 +1,12 @@
+# NOTE: lower() method
+
+"""The lower() method makes a copy of a source string, replaces all upper-case letters with their lower-case counterparts, and returns the string as the result. Again, the source string remains untouched.
+
+If the string doesn't contain any upper-case characters, the method returns the original string.
+
+Note: The lower() method doesn't take any parameters."""
+
+# Demonstrating the lower() method:
+print("SiGmA=60".lower())
+
+# OUTPUT : sigma=60
