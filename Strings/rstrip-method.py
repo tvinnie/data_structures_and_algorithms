3635c1f7de6e1851e94fb931ac90b4d112bd2392
tvinnie <tvinnie77@gmail.com>
2024-06-13 09:31:06 +0300

    rstrip method

---
+++ Strings/rstrip-method.py
@@ -0,0 +1,14 @@
+# NOTE: rstrip() method
+
+# Two variants of the rstrip() method do nearly the same as lstrips, but affect the opposite side of the string.
+
+# Demonstrating the rstrip() method:
+print("[" + " upsilon ".rstrip() + "]")
+print("cisco.com".rstrip(".com"))
+
+# OUTPUT
+"""
+[ upsilon]
+cis
+
+"""
