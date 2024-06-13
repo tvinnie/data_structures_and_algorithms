a0c5e5ff22d918f090e44e3eb7f5726e6d05a257
tvinnie <tvinnie77@gmail.com>
2024-06-13 10:11:40 +0300

    starts with method

---
+++ Strings/starts-with-method.py
@@ -0,0 +1,17 @@
+# NOTE: startswith() method
+
+# The startswith() method is a mirror reflection of endswith() – it checks if a given string starts with the specified substring.
+
+# Demonstrating the startswith() method:
+print("omega".startswith("meg"))
+print("omega".startswith("om"))
+
+print()
+
+# Output
+
+"""
+False
+True
+
+"""
