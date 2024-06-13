58d6993f3026085243669245c023b4c5ecaab07b
tvinnie <tvinnie77@gmail.com>
2024-06-13 10:25:31 +0300

    upper case method

---
+++ Strings/upper-case-method.py
@@ -0,0 +1,8 @@
+# NOTE: upper() method
+
+# The upper() method makes a copy of the source string, replaces all lower-case letters with their upper-case counterparts, and returns the string as the result.
+
+# Demonstrating the upper() method:
+print("I know that I know nothing. Part 2.".upper())
+
+# Output: I KNOW THAT I KNOW NOTHING. PART 2.
