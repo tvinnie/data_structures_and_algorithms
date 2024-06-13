9d9743a36119c1c79c85da4a509326b3bcc7391d
tvinnie <tvinnie77@gmail.com>
2024-06-13 10:19:59 +0300

    swapcase method

---
+++ Strings/swapcase.py
@@ -0,0 +1,13 @@
+# NOTE: swapcase() method
+
+"""The swapcase() method makes a new string by swapping the cases of all letters within the source string: lower-case characters become upper-case, and vice versa.
+
+All other characters remain untouched."""
+
+# Demonstrating the swapcase() method:
+print("I know that I know nothing.".swapcase())
+
+print()
+
+# Output: i KNOW THAT i KNOW NOTHING.
+
