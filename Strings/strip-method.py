32a295c170e63abc622cd06a1cbc9f837edb8087
tvinnie <tvinnie77@gmail.com>
2024-06-13 10:13:37 +0300

    strip method

---
+++ Strings/strip-method.py
@@ -0,0 +1,8 @@
+# NOTE: strip() method
+
+# The strip() method combines the effects caused by rstrip() and lstrip() – it makes a new string lacking all the leading and trailing whitespaces.
+
+# Demonstrating the strip() method:
+print("[" + "   aleph   ".strip() + "]")
+
+# Output:   [aleph]
