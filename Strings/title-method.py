15659600940cc759933158ea7f8df703a9f028da
tvinnie <tvinnie77@gmail.com>
2024-06-13 10:22:19 +0300

    title case method

---
+++ Strings/title-method.py
@@ -0,0 +1,10 @@
+# NOTE: title() method
+
+# The title() method performs a somewhat similar function – it changes every word's first letter to upper-case, turning all other ones to lower-case.
+
+# Demonstrating the title() method:
+print("I know that I know nothing. Part 1.".title())
+
+print()
+
+# Output: I Know That I Know Nothing. Part 1.
