363062bf1dbdedf87d15807ed7465f517c5706b9
tvinnie <tvinnie77@gmail.com>
2024-06-14 15:29:52 +0300

    reading integer values

--- Projects & Labs/Labs/reading-integers-only.py
+++ Projects & Labs/Labs/reading-integers-only.py
@@ -22,3 +22,21 @@ Enter number from -10 to 10: 1
 The number is: 1
 """
 
+def read_int(prompt, min, max):
+    ok = False
+    while not ok:
+        try:
+            value = int(input(prompt))
+            ok = True
+        except ValueError:
+            print("Error: wrong input")
+        if ok:
+            ok = value >= min and value <= max
+        if not ok:
+            print("Error: the value is not within permitted range (" + str(min) + ".." + str(max) + ")")
+    return value;
+
+
+v = read_int("Enter a number from -10 to 10: ", -10, 10)
+
+print("The number is:", v)
