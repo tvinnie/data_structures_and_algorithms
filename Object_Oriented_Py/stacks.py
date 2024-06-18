04441d8dce83caa48de9342984490b253b57abb4
tvinnie <tvinnie77@gmail.com>
2024-06-18 12:48:45 +0300

    stacks

---
+++ Object_Oriented_Py/stacks.py
@@ -0,0 +1,67 @@
+# Procedural Stacks
+
+stack = []
+
+def push(val):
+    stack.append(val)
+
+def pop():
+    val = stack[-1]
+    del stack[-1]
+    return val
+
+push(3)
+push(2)
+push(1)
+
+print(pop())
+print(pop())
+print(pop())
+
+# Output
+"""
+1
+2
+3
+"""
+# --------------------------------------------------------------------------------------
+    
+# Object Oriented Stacks
+
+class Stack:
+    def __init__(self):
+        self.__stack_list = []
+
+
+    def push(self, val):
+        self.__stack_list.append(val)
+
+
+    def pop(self):
+        val = self.__stack_list[-1]
+        del self.__stack_list[-1]
+        return val
+
+
+stack_object = Stack()
+
+stack_object.push(3)
+stack_object.push(2)
+stack_object.push(1)
+
+print(stack_object.pop())
+print(stack_object.pop())
+print(stack_object.pop())
+
+
+# Output : 
+
+"""
+1
+2
+3
+"""
+
+
+# NOTE: When any class component has a name starting with two underscores (__), it becomes private – this means that it can be accessed only from within the class.
+    
