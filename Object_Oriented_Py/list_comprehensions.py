73cc9788d10b1c0a5e59367598f5c6b92c3bdf52
tvinnie <tvinnie77@gmail.com>
2024-06-24 08:56:39 +0300

    updating list comprehension

--- Object_Oriented_Py/list_comprehensions.py
+++ Object_Oriented_Py/list_comprehensions.py
@@ -12,3 +12,12 @@ list_2 = [10 ** ex for ex in range(6)]
 # Output
 print(list_1) # [1, 10, 100, 1000, 10000, 100000]
 print(list_2) # [1, 10, 100, 1000, 10000, 100000]
+
+
+# EXAMPLE 2: LISTS
+
+the_list = [1 if x % 2 == 0 else 0 for x in range(10)]
+
+print(the_list)
+
+# Output: [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
