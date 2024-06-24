3e4da469f48f179b26fcf4f4f47b61c2726fbced
tvinnie <tvinnie77@gmail.com>
2024-06-24 09:41:54 +0300

    lambda functions

---
+++ Object_Oriented_Py/lambdas.py
@@ -0,0 +1,32 @@
+def print_function(args, fun):
+    for x in args:
+        print('f(', x,')=', fun(x), sep='')
+
+
+def poly(x):
+    return 2 * x**2 - 4 * x + 2 #lambda function in a function
+
+
+print_function([x for x in range(-2, 3)], poly) 
+
+# OR
+
+print_function([x for x in range(-2, 3)], lambda x: 2 * x**2 - 4 * x + 2) #lambda inside the print function
+    
+"""
+f(-2)=18
+f(-1)=8
+f(0)=2
+f(1)=0
+f(2)=2
+
+"""
+
+
+# lambda with map() function
+
+for x in map(lambda x: x * x, list_2):
+    print(x, end=' ')
+print()
+
+# Output: 1 4 16 64 256 
