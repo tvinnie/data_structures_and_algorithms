b6a7e2d17274306de6601d5728ec8ae4ef638274
tvinnie <tvinnie77@gmail.com>
2024-06-24 09:42:42 +0300

    lambda functions

--- Object_Oriented_Py/lambdas.py
+++ Object_Oriented_Py/lambdas.py
@@ -25,6 +25,10 @@ f(2)=2
 
 # lambda with map() function
 
+list_1 = [x for x in range(5)]
+list_2 = list(map(lambda x: 2 ** x, list_1))
+print(list_2)
+
 for x in map(lambda x: x * x, list_2):
     print(x, end=' ')
 print()
