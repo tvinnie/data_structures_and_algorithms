482a48b0d8563dafbcd2088fcc7e79fa61c9ab1b
tvinnie <tvinnie77@gmail.com>
2024-06-24 11:14:18 +0300

    closures

---
+++ Object_Oriented_Py/closures.py
@@ -0,0 +1,21 @@
+def outer(par):
+    loc = par
+
+    def inner():
+        return loc
+    return inner
+
+
+var = 1
+fun = outer(var)
+print(fun())
+    
+Output: 1
+
+"""
+Look carefully:
+
+ * the inner() function returns the value of the variable accessible inside its scope, as inner() can use any of the entities at the disposal of outer()
+ * the outer() function returns the inner() function itself; more precisely, it returns a copy of the inner() function, the one which was frozen at the moment of outer()'s invocation; the frozen function contains its full environment, including the state of all local variables, which also means that the value of loc is successfully retained, although outer() ceased to exist a long time ago
+
+"""
