56852889b85c8454098387c711acf68bcc03448a
tvinnie <tvinnie77@gmail.com>
2024-06-28 10:49:05 +0300

    todays date

---
+++ Object_Oriented_Py/Date & Time/current_date_today.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+current_day = date.today()
+
+print("Today:", today)
+print("Year:", today.year)
+print("Month:", today.month)
+print("Day:", today.day)
+
+# OUTPUT:
+"""
+Today: 2024-06-28
+Year: 2024
+Month: 6
+Day: 28
+
+"""
