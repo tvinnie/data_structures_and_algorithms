3639c4e9ebcf0f4abc30b83b51bb37301c3f3e3e
tvinnie <tvinnie77@gmail.com>
2024-07-06 22:18:13 +0300

    print month

--- Object_Oriented_Py/Calendar/specific_month.py
+++ Object_Oriented_Py/Calendar/specific_month.py
@@ -1,3 +1,9 @@
 import calendar
 
 print(calendar.month(2024,12)) # prints the calendar the month of december, year 2024
+
+# Same operation but without the print funtion, using the prmonth() funtion.
+
+import calendar
+
+calendar.prmonth(2024,12)
