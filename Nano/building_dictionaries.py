1d89ba42bf022162b947671a0f900500d2acb15e
tvinnie <tvinnie77@gmail.com>
2024-07-09 09:06:30 +0300

    dictionaries buildup

---
+++ Nano/building_dictionaries.py
@@ -0,0 +1,28 @@
+#  Method 1: Using a for loop to create a set of counters
+
+book_title =  ['great', 'expectations','the', 'adventures', 'of', 'sherlock','holmes','the','great','gasby','hamlet','adventures','of','huckleberry','fin']
+
+word_counter = {}
+
+for word in book_title:
+    if word not in word_counter:
+        word_counter[word] = 1
+    else:
+        word_counter[word] += 1
+
+# OUTPUT: 
+# {'great': 2, 'expectations': 1, 'the': 2, 'adventures': 2, 'of': 2, 'sherlock': 1, 'holmes': 1, 'gasby': 1, 'hamlet': 1, 'huckleberry': 1, 'fin': 1}
+
+# Method 2: Using the get method
+
+book_title =  ['great', 'expectations','the', 'adventures', 'of', 'sherlock','holmes','the','great','gasby','hamlet','adventures','of','huckleberry','fin']
+
+word_counter = {}
+
+for word in book_title:
+    word_counter[word] = word_counter.get(word, 0) + 1
+
+
+# OUTPUT
+
+# {'great': 2, 'expectations': 1, 'the': 2, 'adventures': 2, 'of': 2, 'sherlock': 1, 'holmes': 1, 'gasby': 1, 'hamlet': 1, 'huckleberry': 1, 'fin': 1}
